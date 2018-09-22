409c001d8a7c960fff88c84bec903ad9ea543008
Navlesh <navlesh.agr@gmail.com>
2018-09-22 16:58:37 +0530

    Initial Commit

---
+++ PythonDjangoApp/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Navlesh\Desktop\Example\PythonDjangoApp\PythonDjangoApp\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
